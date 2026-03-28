514bfbb4663ecbda4819f7e7633ca347e8173b83
Ansh Pandya <ansh.pandya@avantika.edu.in>
2026-03-28 15:24:51 +0000

    takes input of the name and days overdue

--- Problem_01.py
+++ Problem_01.py
@@ -1,3 +1,8 @@
 def calculate_fine(book_title, days_overdue , daily_rate = 5.0 , max_fine = 150.0):
     fine = days_overdue * daily_rate 
-    
+    if (fine> max_fine):
+        fine = max_fine 
+    return fine 
+
+book_title = input()
+days_overdue = int(input())
